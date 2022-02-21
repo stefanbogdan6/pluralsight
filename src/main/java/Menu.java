e2b450ebac1d7dc40824817662985de74a91f3dc
Stefan Bogdan <stefan_bogdan.mitrache@yahoo.com>
2022-02-21 14:31:34 +0200

    first-commit

---
+++ src/main/java/Menu.java
@@ -0,0 +1,36 @@
+import java.util.Iterator;
+
+public class Menu extends MenuComponent {
+
+    public Menu(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    //@Override
+    public MenuComponent add(MenuComponent menuComponent) {
+        menuComponents.add(menuComponent);
+        return menuComponent;
+    }
+
+    //@Override
+    public MenuComponent remove(MenuComponent menuComponent) {
+        menuComponents.remove(menuComponent);
+        return menuComponent;
+    }
+
+    public String toString() {
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(print(this));
+
+        Iterator<MenuComponent> itr = menuComponents.iterator();
+
+        while(itr.hasNext()) {
+            MenuComponent menuComponent = itr.next();
+            builder.append(menuComponent.toString());
+        }
+        return builder.toString();
+    }
+}
---
+++ src/main/java/MenuItem.java
@@ -0,0 +1,12 @@
+public class MenuItem extends MenuComponent{
+
+    public MenuItem(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return print(this);
+    }
+}
